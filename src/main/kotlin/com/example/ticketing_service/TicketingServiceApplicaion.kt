17639ba75916f55bf7949b0d9eb4cc8a6b525c60
최경영 <yeong7098@naver.com>
2025-11-24 20:27:56 +0900

    Feat : entity 생성

--- src/main/kotlin/com/example/ticketing_service/TicketingServiceApplicaion.kt
+++ src/main/kotlin/com/example/ticketing_service/TicketingServiceApplicaion.kt
@@ -1,9 +1,11 @@
-package com.example.demo
+package com.example.ticketing_service
 
 import io.github.cdimascio.dotenv.dotenv
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@EnableJpaAuditing
 @SpringBootApplication
 class TicketingServiceApplication
 
