b99a773b53811e8465ff0fb93a795327ad92a1b8
최경영 <yeong7098@naver.com>
2025-11-25 16:33:45 +0900

    Feat : user Serivce

--- src/main/kotlin/com/example/ticketing_service/user/application/UserService.kt
+++ src/main/kotlin/com/example/ticketing_service/user/application/UserService.kt
@@ -1,4 +1,24 @@
 package com.example.ticketing_service.user.application
 
-class UserService {
+import com.example.ticketing_service.user.domain.User
+import com.example.ticketing_service.user.domain.UserRepository
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UserService (
+    private val userRepository: UserRepository
+){
+    @Transactional
+    fun signup(name : String, email : String, password : String) : Long{
+        val newUser = User(name = name, email = email, password = password)
+        return userRepository.save(newUser).id!!
+    }
+
+    @Transactional(readOnly = true)
+    fun getUser(userId : Long) : User {
+      return userRepository.findById(userId)
+          ?: throw IllegalArgumentException("유저가 없습니다")
+    }
 }
