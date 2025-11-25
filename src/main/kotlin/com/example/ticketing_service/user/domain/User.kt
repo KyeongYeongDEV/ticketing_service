a189d7b7df4373c6673e6054a8b2fc76a97a1271
최경영 <yeong7098@naver.com>
2025-11-25 15:44:37 +0900

    Feat : User Entity

---
+++ src/main/kotlin/com/example/ticketing_service/user/domain/User.kt
@@ -0,0 +1,29 @@
+package com.example.ticketing_service.user.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+import com.example.ticketing_service.common.entity.BaseEntity
+
+@Entity
+@Table(name = "users")
+class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    val id: Long? = null,
+
+    @Column(nullable = false, unique = true)
+    val email: String,
+
+    @Column(nullable = false)
+    val password: String,
+
+    @Column(nullable = false)
+    val name: String
+
+) : BaseEntity()
