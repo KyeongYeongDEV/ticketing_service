f8f23064cc29bfbe75f931299f08c44b784a34dd
최경영 <yeong7098@naver.com>
2025-11-27 18:33:56 +0900

    feat(User) : signupRequestDto에 validation 추가

--- src/main/kotlin/com/example/ticketing_service/user/presentation/dto/SignupRequest.kt
+++ src/main/kotlin/com/example/ticketing_service/user/presentation/dto/SignupRequest.kt
@@ -1,11 +1,22 @@
 package com.example.ticketing_service.user.presentation.dto
 
 import com.example.ticketing_service.user.application.dto.SignupCommand
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 
 data class SignupRequest(
+    @field:NotBlank(message = "이름은 필수입니다.")
     val name : String,
+
+    @field:NotBlank(message = "이메일은 필수입니다.")
+    @field:Email(message = "이메일 형식이 올바르지 않습니다")
     val email : String,
+
+    @field:NotBlank(message = "비밀번호는 필수입니다.")
+    @field:Size(min = 6, message = "비밀번호는 6자 이상이여야 합니다..")
     val password : String
+
 ) {
     fun toCommand() : SignupCommand {
         return SignupCommand(
