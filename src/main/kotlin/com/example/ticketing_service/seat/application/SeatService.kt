8bbe3e9addd8c472699d6127ca0e2def43deb962
최경영 <yeong7098@naver.com>
2025-12-19 00:11:47 +0900

    Feat(seat) : Service에 예약을 진행할 때 사용할 비관적 락이 적용된 메서드 추가

--- src/main/kotlin/com/example/ticketing_service/seat/application/SeatService.kt
+++ src/main/kotlin/com/example/ticketing_service/seat/application/SeatService.kt
@@ -1,5 +1,8 @@
 package com.example.ticketing_service.seat.application
 
+import com.example.ticketing_service.global.exception.BusinessException
+import com.example.ticketing_service.global.exception.ErrorCode
+import com.example.ticketing_service.seat.domain.Seat
 import com.example.ticketing_service.seat.domain.SeatStatus
 import com.example.ticketing_service.seat.infra.SeatJpaRepository
 import com.example.ticketing_service.seat.presentation.dto.SeatResponse
@@ -15,5 +18,11 @@ class SeatService(
         // 예약 가능한 좌석만 조회
         return seatRepository.findAllByScheduleIdAndStatus(scheduleId, SeatStatus.AVAILABLE)
             .map { SeatResponse.from(it) }
+    }
+
+    @Transactional
+    fun getSeatForReservation(seatId: Long): Seat {
+        return seatRepository.findByIdWithPessimisticLock(seatId)
+            ?: throw BusinessException(ErrorCode.SEAT_NOT_FOUND)
     }
 }
