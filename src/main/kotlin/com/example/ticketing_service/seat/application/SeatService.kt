2ba8618c96224f5b578dd8c1bf73b3124db0ce81
최경영 <yeong7098@naver.com>
2025-12-20 00:52:40 +0900

    Refactor(seat) : serivce에 비관적 락 적용

--- src/main/kotlin/com/example/ticketing_service/seat/application/SeatService.kt
+++ src/main/kotlin/com/example/ticketing_service/seat/application/SeatService.kt
@@ -3,19 +3,18 @@ package com.example.ticketing_service.seat.application
 import com.example.ticketing_service.global.exception.BusinessException
 import com.example.ticketing_service.global.exception.ErrorCode
 import com.example.ticketing_service.seat.domain.Seat
+import com.example.ticketing_service.seat.domain.SeatRepository
 import com.example.ticketing_service.seat.domain.SeatStatus
-import com.example.ticketing_service.seat.infra.SeatJpaRepository
 import com.example.ticketing_service.seat.presentation.dto.SeatResponse
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class SeatService(
-    private val seatRepository: SeatJpaRepository
+    private val seatRepository: SeatRepository
 ) {
     @Transactional(readOnly = true)
     fun getAvailableSeats(scheduleId: Long): List<SeatResponse> {
-        // 예약 가능한 좌석만 조회
         return seatRepository.findAllByScheduleIdAndStatus(scheduleId, SeatStatus.AVAILABLE)
             .map { SeatResponse.from(it) }
     }
