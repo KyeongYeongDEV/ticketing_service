81672baae37a3b24782670190f48d2e40750feb6
최경영 <yeong7098@naver.com>
2026-01-05 19:19:19 +0900

    Rerfactor : Recovery Batch 시스템을 위한 repo 수정

--- src/main/kotlin/com/example/ticketing_service/payment/infra/PaymentRepositoryImpl.kt
+++ src/main/kotlin/com/example/ticketing_service/payment/infra/PaymentRepositoryImpl.kt
@@ -2,10 +2,14 @@ package com.example.ticketing_service.payment.infra
 
 import com.example.ticketing_service.payment.domain.Payment
 import com.example.ticketing_service.payment.domain.PaymentRepository
+import com.example.ticketing_service.payment.domain.PaymentStatus
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
 
-interface PaymentJpaRepository : JpaRepository<Payment, Long>
+interface PaymentJpaRepository : JpaRepository<Payment, Long> {
+    fun findAllByStatusAndCreatedAtBefore(status: PaymentStatus, timeLimit: LocalDateTime): List<Payment>
+}
 
 @Repository
 class PaymentRepositoryImpl (
@@ -13,5 +17,9 @@ class PaymentRepositoryImpl (
 ) : PaymentRepository {
     override  fun save(payment : Payment) : Payment {
         return jpaRepository.save(payment)
+    }
+
+    override fun findAllByStatusAndCreatedAtBefore(status: PaymentStatus, timeLimit: LocalDateTime): List<Payment> {
+        return jpaRepository.findAllByStatusAndCreatedAtBefore(status, timeLimit)
     }
 }
--- src/main/kotlin/com/example/ticketing_service/queue/scheduler/WaitingQueueScheduler.kt
+++ src/main/kotlin/com/example/ticketing_service/queue/scheduler/WaitingQueueScheduler.kt
@@ -1,38 +1,55 @@
 package com.example.ticketing_service.queue.scheduler
 
+import com.example.ticketing_service.global.common.RedisLockRepository
 import org.slf4j.LoggerFactory
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.Duration
+import java.util.UUID
 
 @Component
 class WaitingQueueScheduler(
-    private val redisTemplate: StringRedisTemplate
+    private val redisTemplate: StringRedisTemplate,
+    private val redisLockRepository: RedisLockRepository
 ) {
     private val log = LoggerFactory.getLogger(this::class.java)
 
     private val WAITING_KEY = "queue:waiting"
     private val ACTIVE_KEY_PREFIX = "queue:active:"
-
-    // 한 번에 입장시킬 인원 수 50명 (트래픽에 따라 조절)
     private val ENTER_LIMIT = 50L
 
     @Scheduled(fixedDelay = 1000) // 1초마다 실행
     fun enterUser() {
-        // 대기열에서 가장 오래 기다린 상위 ENTER_LIMIT 명 조회
-        val usersToEnter = redisTemplate.opsForZSet().range(WAITING_KEY, 0, ENTER_LIMIT - 1)
+        val lockKey = "scheduler:lock"
+        val lockToken = UUID.randomUUID().toString()
 
-        if (usersToEnter.isNullOrEmpty()) return
+        try {
+            // 락 획득 시도 (3초간 유효) -> 실패하면 이번 턴은 스킵
+            if (!redisLockRepository.lock(lockKey, lockToken, 3000)) {
+                return
+            }
 
-        log.info("입장 처리 시작: ${usersToEnter.size}명 진입 허용")
+            //대기열 입장
+            val usersToEnter = redisTemplate.opsForZSet().range(WAITING_KEY, 0, ENTER_LIMIT - 1)
 
-        usersToEnter.forEach { userId ->
-            // 활성 토큰 발급 (유효시간 5분) -> 이제부터 API 호출 가능
-            redisTemplate.opsForValue().set("$ACTIVE_KEY_PREFIX$userId", "ACCESS", Duration.ofMinutes(5))
+            if (!usersToEnter.isNullOrEmpty()) {
+                log.info("입장 처리 시작: ${usersToEnter.size}명 진입 허용")
 
-            // 대기열(Waiting Queue)에서 제거
-            redisTemplate.opsForZSet().remove(WAITING_KEY, userId)
+                usersToEnter.forEach { userId ->
+                    // 활성 토큰 발급 (유효시간 5분)
+                    redisTemplate.opsForValue().set("$ACTIVE_KEY_PREFIX$userId", "ACCESS", Duration.ofMinutes(5))
+
+                    // 대기열 제거
+                    redisTemplate.opsForZSet().remove(WAITING_KEY, userId)
+                }
+            }
+
+        } catch (e: Exception) {
+            log.error("스케줄러 실행 중 에러 발생", e)
+        } finally {
+            // 락 해제
+            redisLockRepository.unlock(lockKey, lockToken)
         }
     }
 }
