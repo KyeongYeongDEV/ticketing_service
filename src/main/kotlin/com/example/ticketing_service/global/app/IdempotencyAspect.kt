aab69798ff41e41067cdff2b23d94b034030f7ab
최경영 <yeong7098@naver.com>
2026-01-05 15:36:59 +0900

    Refactor : 대기 큐 멱등성 정리 - 메서드 실행 중 예외 발생 시 redis 키 제거하여 바로 재요청 받을 수 있게 처리

--- src/main/kotlin/com/example/ticketing_service/global/app/IdempotencyAspect.kt
+++ src/main/kotlin/com/example/ticketing_service/global/app/IdempotencyAspect.kt
@@ -4,8 +4,9 @@ import com.example.ticketing_service.global.annotation.Idempotent
 import com.example.ticketing_service.global.exception.BusinessException
 import com.example.ticketing_service.global.exception.ErrorCode
 import jakarta.servlet.http.HttpServletRequest
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
-import org.aspectj.lang.annotation.Before
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Component
 import org.springframework.web.context.request.RequestContextHolder
@@ -18,8 +19,8 @@ class IdempotencyAspect(
     private val redisTemplate: StringRedisTemplate
 ) {
 
-    @Before("@annotation(idempotent)")
-    fun checkIdempotency(idempotent: Idempotent) {
+    @Around("@annotation(idempotent)")
+    fun checkIdempotency(joinPoint: ProceedingJoinPoint, idempotent: Idempotent): Any? {
         val request: HttpServletRequest = (RequestContextHolder.currentRequestAttributes() as ServletRequestAttributes).request
 
         // 헤더에서 키 추출
@@ -35,9 +36,19 @@ class IdempotencyAspect(
         val isFirstRequest = redisTemplate.opsForValue()
             .setIfAbsent(redisKey, "PROCESSING", Duration.ofMinutes(10))
 
-        // 이미 키가 존재하면(false) 중복 요청으로 간주하고 에러 발생
+        // 이미 키가 존재하면 중복 요청으로 간주하고 에러 발생
         if (isFirstRequest == false) {
-            throw BusinessException(ErrorCode.DUPLICATE_REQUEST) // "이미 처리된 요청입니다."
+            throw BusinessException(ErrorCode.DUPLICATE_REQUEST)
+        }
+
+        // 비즈니스 로직 실행 및 예외 처리
+        try {
+            // 실제 컨트롤러 메서드 실행
+            return joinPoint.proceed()
+        } catch (e: Exception) {
+            // 로직 실패 시 키 삭제
+            redisTemplate.delete(redisKey)
+            throw e
         }
     }
 }
