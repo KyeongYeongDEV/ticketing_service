ffefed94213849cdabe3454667bd709c1aee4c5f
최경영 <yeong7098@naver.com>
2026-01-08 00:13:28 +0900

    Feat(SEE) : 큐 스케줄러 변경 및 redis.conf 리스너 연결

--- src/main/kotlin/com/example/ticketing_service/queue/scheduler/WaitingQueueScheduler.kt
+++ src/main/kotlin/com/example/ticketing_service/queue/scheduler/WaitingQueueScheduler.kt
@@ -19,36 +19,31 @@ class WaitingQueueScheduler(
     private val ACTIVE_KEY_PREFIX = "queue:active:"
     private val ENTER_LIMIT = 50L
 
-    @Scheduled(fixedDelay = 1000) // 1초마다 실행
+    @Scheduled(fixedDelay = 1000)
     fun enterUser() {
         val lockKey = "scheduler:lock"
         val lockToken = UUID.randomUUID().toString()
 
         try {
-            // 락 획득 시도 (3초간 유효) -> 실패하면 이번 턴은 스킵
-            if (!redisLockRepository.lock(lockKey, lockToken, 3000)) {
-                return
-            }
+            if (!redisLockRepository.lock(lockKey, lockToken, 3000)) return
 
-            //대기열 입장
             val usersToEnter = redisTemplate.opsForZSet().range(WAITING_KEY, 0, ENTER_LIMIT - 1)
 
             if (!usersToEnter.isNullOrEmpty()) {
-                log.info("입장 처리 시작: ${usersToEnter.size}명 진입 허용")
-
                 usersToEnter.forEach { userId ->
-                    // 활성 토큰 발급 (유효시간 5분)
+                    // 활성 토큰 발급
                     redisTemplate.opsForValue().set("$ACTIVE_KEY_PREFIX$userId", "ACCESS", Duration.ofMinutes(5))
-
                     // 대기열 제거
                     redisTemplate.opsForZSet().remove(WAITING_KEY, userId)
+
+                    // Redis Pub/Sub으로 알림 발송
+                    // "queue:entry" 채널에 userId를 던지면, 모든 서버의 리스너가 듣습니다.
+                    redisTemplate.convertAndSend("queue:entry", userId)
                 }
             }
-
         } catch (e: Exception) {
-            log.error("스케줄러 실행 중 에러 발생", e)
+            log.error("스케줄러 에러", e)
         } finally {
-            // 락 해제
             redisLockRepository.unlock(lockKey, lockToken)
         }
     }
