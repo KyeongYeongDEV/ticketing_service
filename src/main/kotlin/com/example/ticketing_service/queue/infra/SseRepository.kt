b30dbf80869f4a0717cd9fb61e5cd5fe4e65dee8
최경영 <yeong7098@naver.com>
2026-01-10 00:15:39 +0900

    Refactor : SSE 타임아웃 및 로그 추가

--- src/main/kotlin/com/example/ticketing_service/queue/infra/SseRepository.kt
+++ src/main/kotlin/com/example/ticketing_service/queue/infra/SseRepository.kt
@@ -1,5 +1,6 @@
 package com.example.ticketing_service.queue.infra
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 import java.util.concurrent.ConcurrentHashMap
@@ -8,16 +9,30 @@ import java.util.concurrent.ConcurrentHashMap
 class SseRepository {
     // 동시성 문제를 방지하기 위해 ConcurrentHashMap 사용
     private val emitters = ConcurrentHashMap<String, SseEmitter>()
+    private val logger = LoggerFactory.getLogger(this::class.java)
 
-    fun save(userId: String, emitter: SseEmitter) {
+    fun save(userId: String, emitter: SseEmitter): SseEmitter {
+        // 타임아웃  Map에서 제거하는 콜백 등록
+        emitter.onCompletion {
+            logger.info("SSE 연결 종료 (Completed): $userId")
+            this.delete(userId)
+        }
+        emitter.onTimeout {
+            logger.warn("SSE 연결 타임아웃 (Timed Out): $userId")
+            emitter.complete() // complete()를 호출하면 onCompletion이 이어서 실행됨
+        }
+
         emitters[userId] = emitter
-    }
-
-    fun delete(userId: String) {
-        emitters.remove(userId)
+        logger.info("SseEmitter 저장 완료. 현재 접속자 수: ${emitters.size}, User: $userId")
+        return emitter
     }
 
     fun get(userId: String): SseEmitter? {
         return emitters[userId]
+    }
+
+    fun delete(userId: String) {
+        emitters.remove(userId)
+        logger.debug("SseEmitter 삭제 완료: $userId")
     }
 }
