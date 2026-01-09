ce5b68da4ac8c224bf44fa2e0cb1eedf7f19ce02
최경영 <yeong7098@naver.com>
2026-01-10 00:31:35 +0900

    Refactor : Listener 로깅 추가, try catch를 이용해 전송 실패 연결 삭제 처리, 다른 서버와 연결된 유저 예외처리

--- src/main/kotlin/com/example/ticketing_service/queue/infra/RedisQueueListener.kt
+++ src/main/kotlin/com/example/ticketing_service/queue/infra/RedisQueueListener.kt
@@ -1,35 +1,41 @@
 package com.example.ticketing_service.queue.infra
 
+import com.example.ticketing_service.global.common.RedisChannel
+import org.slf4j.LoggerFactory
 import org.springframework.data.redis.connection.Message
 import org.springframework.data.redis.connection.MessageListener
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
+import java.nio.charset.StandardCharsets
 
 @Component
 class RedisQueueListener(
     private val sseRepository: SseRepository
 ) : MessageListener {
 
-    override fun onMessage(message: Message, pattern: ByteArray?) {
-        // Redis 채널에서 메시지(userId)가 옴
-        val userId = String(message.body)
+    private val logger = LoggerFactory.getLogger(this::class.java)
 
-        // 내 서버에 연결된 유저인지 확인
+    override fun onMessage(message: Message, pattern: ByteArray?) {
+        val userId = String(message.body, StandardCharsets.UTF_8)
+        logger.debug("[Redis Sub] 브로드캐스트 메시지 수신 - User ID: $userId")
+
         val emitter = sseRepository.get(userId)
 
         if (emitter != null) {
+            // 알림 전송
+            logger.info("[SSE] 유저($userId)가 이 서버에 존재합니다. 입장 알림 전송.")
             try {
-                // 사용자에게 알림 전송
-                emitter.send(
-                    SseEmitter.event()
-                        .name("entry") // 이벤트 이름
-                        .data("입장 가능") // 데이터
-                )
-                // 알림 보냈으니 연결 종료
+                emitter.send(SseEmitter.event().name(RedisChannel.ENTRY).data("입장 가능합니다."))
+                // 연결 종료
                 emitter.complete()
             } catch (e: Exception) {
+                logger.error("SSE 전송 실패 - User: $userId", e)
+                // 전송에 실패한 죽은 연결 삭제
                 sseRepository.delete(userId)
             }
+        } else {
+            // 다른 서버와 연결된 유저
+            logger.debug("[SSE] 유저($userId)는 이 서버에 없습니다. 무시합니다.")
         }
     }
 }
