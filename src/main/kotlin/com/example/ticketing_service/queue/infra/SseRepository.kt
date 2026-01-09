615ac450fdc9ec44671db52b64388e204206f555
최경영 <yeong7098@naver.com>
2026-01-10 00:41:28 +0900

    Refactor(queue) : SSE 재접속 시 race condition 및 메모리 누수 해결

--- src/main/kotlin/com/example/ticketing_service/queue/infra/SseRepository.kt
+++ src/main/kotlin/com/example/ticketing_service/queue/infra/SseRepository.kt
@@ -7,23 +7,37 @@ import java.util.concurrent.ConcurrentHashMap
 
 @Repository
 class SseRepository {
-    // 동시성 문제를 방지하기 위해 ConcurrentHashMap 사용
-    private val emitters = ConcurrentHashMap<String, SseEmitter>()
     private val logger = LoggerFactory.getLogger(this::class.java)
+    private val emitters = ConcurrentHashMap<String, SseEmitter>()
 
     fun save(userId: String, emitter: SseEmitter): SseEmitter {
-        // 타임아웃  Map에서 제거하는 콜백 등록
-        emitter.onCompletion {
-            logger.info("SSE 연결 종료 (Completed): $userId")
-            this.delete(userId)
-        }
-        emitter.onTimeout {
-            logger.warn("SSE 연결 타임아웃 (Timed Out): $userId")
-            emitter.complete() // complete()를 호출하면 onCompletion이 이어서 실행됨
+        // 새 연결 들어올 시 기존 연결은 만료
+        emitters[userId]?.let { oldEmitter ->
+            logger.warn("중복 로그인 감지 - 기존 연결 종료: $userId")
+            oldEmitter.complete()
         }
 
+        // 콜백 등록
+        emitter.onCompletion {
+            logger.info("SSE 연결 종료 (Completed): $userId")
+            // 맵에 저장된 객체가 '현재 이 emitter'일 때만 삭제
+            emitters.remove(userId, emitter)
+        }
+
+        emitter.onTimeout {
+            logger.warn("SSE 연결 타임아웃 (Timed Out): $userId")
+            emitter.complete()
+        }
+
+        //  onError 추가 - 네트워크 에러 등으로 끊겼을 때
+        emitter.onError { e ->
+            logger.error("SSE 연결 에러 (Error): $userId", e)
+            emitter.complete()
+        }
+
+        // 저장
         emitters[userId] = emitter
-        logger.info("SseEmitter 저장 완료. 현재 접속자 수: ${emitters.size}, User: $userId")
+        logger.info("SseEmitter 저장 완료. User: $userId")
         return emitter
     }
 
@@ -33,6 +47,5 @@ class SseRepository {
 
     fun delete(userId: String) {
         emitters.remove(userId)
-        logger.debug("SseEmitter 삭제 완료: $userId")
     }
 }
